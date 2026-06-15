2791f526cf440d8b4472f95e3f355ab5af8590b0
anshmahajan26 <anshmahajan678@gmail.com>
2026-06-15 15:24:37 +0530

    upload dist in backend

---
+++ frontend/dist/assets/input-D1luYaBI.js
@@ -0,0 +1,1 @@
+import{j as t}from"./vendor-react-DQtI5-R4.js";import{c as n}from"./index-ZFKx9DNH.js";const a=({className:o,...r})=>t.jsx("input",{className:n("h-10 w-full rounded-md border border-border bg-background px-3 py-2 text-sm outline-none transition focus:border-accent",o),...r});export{a as I};
