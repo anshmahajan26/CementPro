6b011dc9db9c5b122ca27097528888d877cd2218
anshmahajan26 <anshmahajan678@gmail.com>
2026-06-22 17:35:53 +0530

    done

--- frontend/dist/assets/input-D80RAaao.js
+++
@@ -1,1 +0,0 @@
-import{aD as t}from"./vendor-xynfUH1O.js";import{c as n}from"./index-BMhfxcuJ.js";const a=({className:o,...r})=>t.jsx("input",{className:n("h-10 w-full rounded-md border border-border bg-background px-3 py-2 text-sm outline-none transition focus:border-accent",o),...r});export{a as I};
